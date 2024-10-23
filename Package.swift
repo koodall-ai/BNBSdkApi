722c9a4ef00ae940e3753fc6b9948f2d1bea71d4
koodall-ai <github-admins@koodall.ai>
2024-10-23 14:56:09 -0400

    Koodall Sdk BNBSdkApi swift package

---
+++ Package.swift
@@ -0,0 +1,42 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let bnbPackageVersion: Version = "1.14.1-162-gcda6b9c4b"
+
+let package = Package(
+    name: "BNBSdkApi",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "BNBSdkApi",
+            targets: [
+                "BNBSdkApi",
+                "BNBSdkApi_BNBSdkCore"
+            ]
+        ),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/sdk-banuba/BNBSdkCore.git",
+            .exact(bnbPackageVersion)
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "BNBSdkApi",
+            url: "https://d2cm7wtcqqw29.cloudfront.net/1.14.1-162-gcda6b9c4b/BNBSdkApi.zip",
+            checksum: "675fc24b6caefa723ddcc76e39aee3469dfd00811be653d2894e8e16dd7a23ab"
+        ),
+        .target(
+            name: "BNBSdkApi_BNBSdkCore",
+            dependencies: [
+                .product(
+                    name: "BNBSdkCore",
+                    package: "BNBSdkCore"
+                ),
+            ]
+        ),
+    ]
+)
---
+++ README.md
@@ -0,0 +1,10 @@
+# Koodall Sdk BNBSdkApi swift package
+[![](https://www.koodall.ai/media/images/logo/logo-color.svg)](https://www.koodall.ai/)
+
+
+### About packages see [HERE](https://docs.kooall.ai/face-ar-sdk-v1/ios/ios_packages)
+
+#### Package version: **1.14.1-162-gcda6b9c4b**
+
+#### **[Koodall Homepage](https://www.koodall.ai)**
+
